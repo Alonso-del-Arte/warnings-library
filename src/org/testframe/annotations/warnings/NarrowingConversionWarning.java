21cee487044ac987ce5b5bfaa5736f815189d04e
Alonso del Arte <alonso.delarte@gmail.com>
2024-10-09 00:26:18 -0400

    Moved to org.testframe.annotations.warnings package

--- src/org/testframe/annotations/warnings/NarrowingConversionWarning.java
+++ src/org/testframe/annotations/warnings/NarrowingConversionWarning.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package org.testframe.warnings;
+package org.testframe.annotations.warnings;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
