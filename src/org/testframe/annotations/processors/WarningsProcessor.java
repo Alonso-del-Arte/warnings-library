c1e0e799da013cd590090fa81bc33d8eca430241
Alonso del Arte <alonso.delarte@gmail.com>
2024-10-18 14:52:51 -0400

    License header, Javadoc, stub change

--- src/org/testframe/annotations/processors/WarningsProcessor.java
+++ src/org/testframe/annotations/processors/WarningsProcessor.java
@@ -1,7 +1,18 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (C) 2024 Alonso del Arte
+ *
+ * This program is free software: you can redistribute it and/or modify it under 
+ * the terms of the GNU General Public License as published by the Free Software 
+ * Foundation, either version 3 of the License, or (at your option) any later 
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more 
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with 
+ * this program. If not, see <http://www.gnu.org/licenses/>.
  */
 package org.testframe.annotations.processors;
 
@@ -15,7 +26,7 @@ import javax.lang.model.element.TypeElement;
 import javax.tools.Diagnostic.Kind;
 
 /**
- *
+ * Processes warnings from the {@code org.testframe.warnings} package.
  * @author Alonso del Arte
  */
 @SupportedAnnotationTypes("org.testframe.warnings.NarrowingConversionWarning")
@@ -30,7 +41,7 @@ public class WarningsProcessor extends AbstractProcessor {
                     -> roundEnv.getElementsAnnotatedWith(typeElem))
                     .forEachOrdered((elements) -> {
                 elements.forEach((element) -> {
-                    this.processingEnv.getMessager().printMessage(Kind.WARNING,
+                    this.processingEnv.getMessager().printMessage(Kind.NOTE,
                             String.format("%s : HAVEN'T WRITTEN TESTS YET %s",
                                     roundEnv.getRootElements(), element), 
                             element);
