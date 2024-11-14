edb802c1ccfef0a9c60e96452e5a45fff09ad9f4
Alonso del Arte <alonso.delarte@gmail.com>
2024-11-13 22:10:35 -0500

    Failing supported annotation types test

--- test/org/testframe/annotations/processors/WarningsProcessorTest.java
+++ test/org/testframe/annotations/processors/WarningsProcessorTest.java
@@ -16,18 +16,48 @@
  */
 package org.testframe.annotations.processors;
 
+import java.lang.annotation.Annotation;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Set;
+
+import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.ProcessingEnvironment;
 import javax.annotation.processing.RoundEnvironment;
+import javax.annotation.processing.SupportedAnnotationTypes;
+import javax.lang.model.element.Element;
 import javax.lang.model.element.TypeElement;
+import javax.tools.Diagnostic.Kind;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+import static org.testframe.api.Asserters.assertContainsSame;
+
 /**
- *
+ * Tests of the WarningsProcessor class.
  * @author Alonso del Arte
  */
 public class WarningsProcessorTest {
+    
+    @Test
+    public void testSupportedAnnotationTypes() throws ClassNotFoundException {
+        ClassLoader loader = ClassLoader.getSystemClassLoader();
+        String name = "org.testframe.annotations.processors.WarningsProcessor";
+        Class<?> type = loader.loadClass(name);
+        SupportedAnnotationTypes annotation 
+                = type.getAnnotation(SupportedAnnotationTypes.class);
+        assert annotation != null : "Annotation should not be null";
+        String customWarnName = "org.testframe.warnings.CustomWarning";
+        String narConvWarnName 
+                = "org.testframe.warnings.NarrowingConversionWarning";
+        String[] expected = {customWarnName, narConvWarnName};
+        String[] actual = annotation.value();
+        assertContainsSame(expected, actual);
+    }
     
     /**
      * Test of process method, of class WarningsProcessor.
