788330ecf377710858f6947c0ae79eae8970e183
Alonso del Arte <alonso.delarte@gmail.com>
2024-12-18 00:51:34 -0500

    Failing no locale in effect test

--- test/org/testframe/annotations/processors/MockProcEnvTest.java
+++ test/org/testframe/annotations/processors/MockProcEnvTest.java
@@ -17,21 +17,37 @@
 package org.testframe.annotations.processors;
 
 import java.lang.annotation.Annotation;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Locale;
+import java.util.Map;
 import java.util.Set;
 
 import javax.annotation.processing.Filer;
+import javax.annotation.processing.Messager;
 import javax.annotation.processing.ProcessingEnvironment;
 import javax.annotation.processing.RoundEnvironment;
+import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.TypeElement;
 import javax.lang.model.util.Elements;
+import javax.lang.model.util.Types;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
 
 /**
- *
+ * Tests of the MockProcEnv class.
  * @author Alonso del Arte
  */
 public class MockProcEnvTest {
+    
+    @Test
+    public void testNoLocaleInEffect() {
+        ProcessingEnvironment instance = new MockProcEnv(new MockMessager());
+        Locale locale = instance.getLocale();
+        String message = "No locale should be in effect";
+        assertNull(message, locale);
+    }
     
 }
