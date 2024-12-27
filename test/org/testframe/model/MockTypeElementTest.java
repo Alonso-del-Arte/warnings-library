c67d3c56cd055984ec61c7948bf9e8704dec9786
Alonso del Arte <alonso.delarte@gmail.com>
2024-12-27 15:47:03 -0500

    Failing getQualifiedName( ) test

--- test/org/testframe/model/MockTypeElementTest.java
+++ test/org/testframe/model/MockTypeElementTest.java
@@ -17,9 +17,11 @@
 package org.testframe.model;
 
 import java.lang.annotation.Annotation;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
-import java.util.Set;
 
 import javax.lang.model.element.AnnotationMirror;
 import javax.lang.model.element.Element;
@@ -35,15 +37,50 @@ import javax.lang.model.type.TypeMirror;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+import static org.testframe.model.MockElementTest.RANDOM;
+
 /**
  * Tests of the MockTypeElement class.
  * @author Alonso del Arte
  */
 public class MockTypeElementTest {
     
+    private static List<Object> makeList() {
+        int size = RANDOM.nextInt(64) + 16;
+        List<Object> list = new ArrayList<>(size);
+        for (int i = 0; i < size; i++) {
+            switch (i % 5) {
+                case 0:
+                    list.add(LocalDate.now().minusDays(i));
+                    break;
+                case 1:
+                    list.add(LocalDateTime.now().minusMinutes(i));
+                    break;
+                case 2:
+                    list.add(new MockElementTest());
+                    break;
+                case 3:
+                    list.add(new MockNameTest());
+                    break;
+                default:
+                    list.add(new Object());
+            }
+        }
+        return list;
+    }
+    
     @Test
-    public void placeholder() {
-        fail("TEST PLACEHOLDER");
+    public void testGetQualifiedName() {
+        System.out.println("getQualifiedName");
+        List<Object> list = makeList();
+        Collections.shuffle(list);
+        list.add(this);
+        list.stream().map((obj) -> obj.getClass()).forEachOrdered((type) -> {
+            TypeElement instance = new MockTypeElement(type);
+            Name expected = new MockName(type);
+            Name actual = instance.getQualifiedName();
+            assertEquals(expected, actual);
+        });
     }
     
 }
