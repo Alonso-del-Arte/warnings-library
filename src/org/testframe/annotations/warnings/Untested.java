f0d09aeafb5608b4df56e8c1d3752ee7c874dd7a
Alonso del Arte <alonso.delarte@gmail.com>
2024-12-31 18:44:36 -0500

    Clarified Javadoc slightly

--- src/org/testframe/annotations/warnings/Untested.java
+++ src/org/testframe/annotations/warnings/Untested.java
@@ -25,12 +25,12 @@ import java.lang.annotation.Target;
  * Warns that a function or procedure (or "method"), or a constructor, has not 
  * been tested. Generally this annotation should only be used when the 
  * developers expect a long lag between first declaring the untested item and 
- * writing the very first test. Still, it's preferable to using a {@link 
- * CustomWarning} with the message that the annotated item has not been tested. 
- * However, this annotation is not available at the class level. My thinking is 
- * that it might be much easier to forget to remove this annotation when it no 
- * longer applies at the class level (which would be as soon as anything it 
- * contains has a test written).
+ * writing the very first test for that item. Still, it's preferable to using a 
+ * {@link CustomWarning} with the message that the annotated item has not been 
+ * tested. However, this annotation is not available at the class level. My 
+ * thinking is that it might be much easier to forget to remove this annotation 
+ * when it no longer applies at the class level (which could be considered to be 
+ * as soon as anything it contains has a test written).
  * @author Alonso del Arte
  */
 @Retention(RetentionPolicy.SOURCE)
