fa4f73697f46a2b3a96eff531a609bfe2638a452
Alonso del Arte <alonso.delarte@gmail.com>
2025-01-03 22:40:41 -0500

    New package under Test Packages

---
+++ test/org/testframe/annotations/warnings/MockAnnotationsProvider.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright (C) 2025 Alonso del Arte
+ *
+ * This program is free software: you can redistribute it and/or modify it under 
+ * the terms of the GNU General Public License as published by the Free Software 
+ * Foundation, either version 3 of the License, or (at your option) any later 
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more 
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with 
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.testframe.annotations.warnings;
+
+/**
+ *
+ * @author Alonso del Arte
+ */
+public class MockAnnotationsProvider {
+    
+    // TODO: Write tests for this
+    @Untested
+    static CustomWarning makeCustomWarning() {
+        return null;
+    }
+    
+    // TODO: Write tests for this
+    @Untested
+    static MockAnnotation makeMockAnnotation() {
+        return null;
+    }
+    
+    // TODO: Write tests for this
+    @Untested
+    static NarrowingConversionWarning makeNarrowingWarning() {
+        return null;
+    }
+    
+    // TODO: Write tests for this
+    @Untested
+    static Untested makeUntestedWarning() {
+        return null;
+    }
+    
+    // TODO: Write tests for this
+    @Untested
+    static Deprecated makeDeprecatedWarning() {
+        return null;
+    }
+    
+    // TODO: Write tests for this
+    @Untested
+    static FunctionalInterface makeFunctionalInterfaceAnnotation() {
+        return null;
+    }
+    
+    // TODO: Write tests for this
+    @Untested
+    static Override makeOverrideAnnotation() {
+        return null;
+    }
+    
+    // TODO: Write tests for this
+    @Untested
+    static SafeVarargs makeSafeVarargsAnnotation() {
+        return null;
+    }
+    
+    // TODO: Write tests for this
+    @Untested
+    static SuppressWarnings makeSuppressWarningsAnnotation() {
+        return null;
+    }
+    
+}
