237f4d6837f7afaa86907ea905ffd94637242342
Alonso del Arte <alonso.delarte@gmail.com>
2025-01-28 23:54:36 -0500

    Failing process untested test

--- test/org/testframe/annotations/processors/WarningsProcessorTest.java
+++ test/org/testframe/annotations/processors/WarningsProcessorTest.java
@@ -166,4 +166,29 @@ public class WarningsProcessorTest {
         assertEquals(expected, actual);
     }
     
+    @Test
+    public void testProcessUntestedWarning() {
+        Untested warning = MockAnnotationsProvider.makeUntestedWarning();
+        String expected = "The called function has not been tested";
+        Annotation[] anns = {warning};
+        Element element = new MockElement(anns);
+        Set<Element> elemSet = new HashSet<>();
+        elemSet.add(element);
+        RoundEnvironment roundEnv = new MockRoundEnv(elemSet);
+        TypeElement typeElem = new MockTypeElement(Untested.class);
+        Set<TypeElement> annotations = new HashSet<>();
+        annotations.add(typeElem);
+        WarningsProcessor instance = new WarningsProcessor();
+        MockMessager messager = new MockMessager();
+        ProcessingEnvironment proc = new MockProcEnv(messager);
+        instance.init(proc);
+        instance.process(annotations, roundEnv);
+        MessageRecord record = messager.getLatestMessage();
+        assert record != null : "Latest message should not be null";
+        String kindMsg = "Message should be of type " + Kind.WARNING.toString();
+        assertEquals(kindMsg, Kind.WARNING, record.getDiagnosticKind());
+        CharSequence actual = record.getMessage();
+        assertEquals(expected, actual);
+    }
+    
 }
